33248e19cfaac921f9a8b8f6b79cfa93772fc207
Dev Patel <devpatel8907@gmail.com>
2021-08-12 16:52:11 +0530

    Initial Commit

---
+++ DS/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  DS
+//
+//  Created by Dev Patel on 12/08/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
